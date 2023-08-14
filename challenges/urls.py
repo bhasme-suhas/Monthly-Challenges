72a09bfeb33567338b8ae87efb640d69a16dda4c
Suhas Bhasme <suhasbhasme10@gmail.com>
2023-08-14 14:57:00 +0530

    new_challenges

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index)
-    path("february", views.suhas)
+    path("",views.index, name = "index"),
+    path("<int:month>",views.monthly_number),
+    path("<str:month>",views.monthly_challenge, name = "m-challenge")   
 ]
-
-
--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
+
+
+    
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": None
+}
 
 def index(request):
-    return HttpResponse("This works!")
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    
+    return render(request, "challenges/index.html", {
+            "months" : months
+        })
 
-def suhas(request):
-    return HttpResponse("1 closer!")
+def monthly_number(request,month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("<h1>Invalid month</h1>")
+    
+    redirect_month = months[month-1]
+    redirect_path = reverse("m-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request,"challenges/challenge.html", {
+        "text": challenge_text,
+        "mont_nnews": month
+        
+        })
+        
+    except:
+        raise Http404()
+    
+    
+    
